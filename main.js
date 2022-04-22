1a5d65032067e6d535003927baffa715d8bc2fb1
Anton <anton.tajudin@gmail.com>
2022-04-22 15:50:21 +0700

    css dan html dan js

--- main.js
+++ main.js
@@ -8,13 +8,25 @@ start();
 
 function createBreedList(breedList) {
   document.getElementById('breed').innerHTML = /*html*/ `
-   <select>
+   <select onchange="loadByBreed(this.value)">
     <option>Choose a dog breed</option>
     ${Object.keys(breedList)
       .map((breed) => {
         return `<option>${breed}</option>`;
       })
       .join('')}}
-  </select>
-`;
+  </select>`;
+}
+
+async function loadByBreed(breed) {
+  if (breed != 'Choose a dog breed') {
+    const res = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+    const data = await res.json();
+    createSlideshow(data.message);
+  }
+}
+
+function createSlideshow(images) {
+  document.getElementById('slideshow').innerHTML = /*html*/ `
+  <div class="slide" style="background-image: url('${images[0]}');"></div>`;
 }
